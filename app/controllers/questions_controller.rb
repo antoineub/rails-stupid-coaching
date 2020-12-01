52237e4d1da5d8a3ec66e6929b1dcfd559d90dd8
Antoine Ung Bao <antoine.ungbao@gmail.com>
2020-12-01 21:37:48 +0100

    session end

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @coach = coach(@question)
+    
+  end
+
+  private
+  
+  def coach(question)
+    if question == "I am going to work"
+      "I am going to work"
+    elsif question.include? "?"
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+
+end
